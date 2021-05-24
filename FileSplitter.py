170a3859db5c06a3900d1298317a44ba17d2123c
speedo2k <speedo2k@interia.pl>
2021-05-24 15:47:45 +0200

    Command line version with split/join based on lines amount or size

--- FileSplitter.py
+++ FileSplitter.py
@@ -1,29 +1,46 @@
 import sys
+import argparse
 
-import sys
+cli_parser = None
+
+def arg_parse():
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument("-j", "--join", help="join files (default: split)", action="store_true")
+    cli_parser.add_argument('file_name', help='file_name for split or join')
+    cli_parser.add_argument("-s", "--size", type=int, help='file size when split')
+    cli_parser.add_argument("-l", "--lines", type=int, help='lines iin file when split')
+    cli_parser.add_argument("-wd", help='working directory for split and join(not implemented yet)')
+    return cli_parser.parse_args()
+
+
 
 class FileSplitter:
     def __init__(self, input_name, output_name, max_size=None, max_lines=None):
         self.input_name = input_name
         self.output_name = output_name
-        self.max_size = max_size
-        if self.max_size is None or self.max_size == 0:
-           self. max_size = sys.maxsize
-        self.max_lines = max_lines
-        if self.max_lines is None or self.max_lines == 0:
-            self.max_lines = sys.maxsize
+        self.max_size = max_size or sys.maxsize
+        self.chunk_size = min(8192, self.max_size)
+        self.max_lines = max_lines or sys.maxsize
+        self.is_text_file = self.max_lines < sys.maxsize
 
-    def reader(self, receiver):
+    def txt_reader(self, receiver, chunk_size=8192):
         r = receiver()
         r.send(None)
         with open(self.input_name, 'r') as input:
             for line in input:
                 r.send(line)
 
+    def bin_reader(self, receiver, chunk_size=8192):
+        r = receiver()
+        r.send(None)
+        with open(self.input_name, 'rb') as input:
+            while chunk := (input.read(chunk_size)):
+                r.send(chunk)
+
     def out_fn_seq(self, name, seq):
         return name+'.'+str(seq).zfill(3)
 
-    def writer(self):
+    def txt_writer(self):
         fseq = 0
         try:
             while True:
@@ -39,24 +56,46 @@ class FileSplitter:
         except GeneratorExit:
             pass
 
-    def split(self):
-        self.reader(self.writer)
+    def bin_writer(self):
+        fseq = 0
+        try:
+            while True:
+                with  open(self.out_fn_seq(self.output_name, fseq), 'wb') as output:
+                    size = 0
+                    fseq +=1
+                    while size < self.max_size:
+                        chunk = (yield)
+                        output.write(chunk)
+                        size += len(chunk)
+        except GeneratorExit:
+            pass
 
-    def join(self):
-        with open(self.input_name+'.all', 'w') as output:
+
+    def split(self):
+        if self.is_text_file:
+            self.txt_reader(self.txt_writer)
+        else:
+            self.bin_reader(self.bin_writer, chunk_size=self.chunk_size)
+
+    def join(self, chunk_size=8192):
+        with open(self.input_name+'.all', 'wb') as output:
             fseq = 0
             try:
                 while True:
-                    with open(self.out_fn_seq(self.output_name, fseq), 'r') as input:
-                        for line in input:
-                            output.write(line)
+                    with open(self.out_fn_seq(self.output_name, fseq), 'rb') as input:
+                        while chunk := input.read(chunk_size):
+                            output.write(chunk)
                     fseq +=1
             except FileNotFoundError:
                 pass
 
+
 if __name__ == '__main__':
-
-
-    fs = FileSplitter('textfilesplitter.py', 'test.pyf', max_lines=10)
-    fs.split()
-    fs.join()
+    args = arg_parse()
+    fn =  args.file_name
+    fs = FileSplitter(fn, fn, max_lines=args.lines, max_size=args.size)
+    print (f'Fn {fn}, action:{args.join}, lines={args.lines}, size={args.size}')
+    if args.join:
+        fs.join()
+    else:
+        fs.split()
